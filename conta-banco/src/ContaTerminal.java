4b72d6ceaee67a709d50ba8bf69d242711b95665
Aylla Scaglia <aylla@scaglia.com.br>
2025-02-04 12:16:33 -0300

    Desafio completo

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nameCliente = input.nextLine();
+        System.out.print("Digite o nome da sua agencia: ");
+        String numAgencia = input.nextLine();
+        System.out.print("Digite o número da sua conta: ");
+        int numConta = input.nextInt();
+        System.out.print("Digite o valor que deseja depositar: ");
+        float saldo = input.nextFloat();
+
+        System.out.println(
+            "Olá " + nameCliente + 
+            ", obrigado por criar uma conta em nosso banco, sua agência é " 
+            + numAgencia + ", conta " + numConta +
+            " e seu saldo " + saldo + " já está disponível para saque"
+        );
     }
 }
+
